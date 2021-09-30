bbc87d1d2c6a7d288114b8636e0abc452343aee7
SeekerRook <xrist.vardakis@gmail.com>
2021-09-30 14:49:22 +0300

    finished GUI version (608ui.py)

--- 608ui.py
+++ 608ui.py
@@ -1,57 +1,100 @@
 import random
 import pygame as pg
+from time import sleep
+from tkinter import *
+from tkinter import messagebox
 X = 10
 Y = 10
 x = random.randint(0,X-1)
 y = random.randint( 0,Y-1)
 size = 500
-def main():
-	pg.init()
+Mess = ""
+flag = False
+logo = 50
+bottom = 100
 
-	DISPLAY=pg.display.set_mode((size,size),0,32)
+color1 = (132, 165, 184) #header
+color2 = (66,104,124) #background
+color3 = (179, 218, 241) #boards
+def reset(DISPLAY):
+	
+
 	pg.display.set_caption("608")
-	background=(200,200,250)
-    
+	background=color2
 
+		
 	DISPLAY.fill(background)
+	font = pg.font.Font(None, 50)
+	textlogo = font.render("608", 1, color1)
+	DISPLAY.blit(textlogo,(size//2-55,0))
+	global Mess
+	global x
+	global y
+	x = random.randint(0,X-1)
+	y = random.randint( 0,Y-1)
+	Mess = ""
+	return DISPLAY
+def render(DISPLAY):
+	global Mess
+	global x 
+	global y 
+	
+
+	print (Mess)
 	for j in range(0,X+1):
 		for i in range(0,X+1):
 			if (i == x and j == y ):
 				message = "608"
 			else: message = "609"
 			font = pg.font.Font(None, 30)
-			text = font.render(message, 1, (0, 0, 50))
+			text = font.render(message, 1, color3)
 
-			DISPLAY.blit(text,(((size/ X) * i ) + 5 ,((size/X) * j)+ 10))
+			DISPLAY.blit(text,(((size/ X) * i ) + 5 ,((size/X) * j)+ 10+logo))
+		
+
+			DISPLAY.blit(text,(((size/ X) * i ) + 5 ,((size/X) * j)+ 10+logo))
+		text2 = font.render(Mess, 1, color1)
+		DISPLAY.blit(text2,(size//2,size+bottom//2+logo))
+
+def main():
+	pg.init()
+	DISPLAY=pg.display.set_mode((size ,size+bottom+ logo),0,32)	
+	reset(DISPLAY)
+	render(DISPLAY)
 	while True:
 		for event in pg.event.get():
-			if event.type == pg.QUIT:
-				pg.quit()
-    			
-			pg.display.update()
+				if event.type == pg.QUIT:
+					print("bye")
+					pg.quit()
+				if event.type == pg.MOUSEBUTTONUP:	
+					onclick()
+					print(Mess)
+					render(DISPLAY)
+					pg.display.update()
+					global flag
+				if flag:
+					flag = False					
+					print("sleep")
+					sleep (1)
+					print("slept")
+					
+					reset(DISPLAY)
+					print("reset")
+					render(DISPLAY)
+					print ("rendered")
+				pg.display.update()
+				
 
 def onclick():
-	x,y = pg.mouse.get_pos()
-	i1 = (x-5)/size*X
-	i  
+	xi,yi= pg.mouse.get_pos()
+	i1 = int((xi-5)/size*X)
+	i2 = int((yi-10-logo)/size*Y)
+	print (i1 , " ", i2)
+	if i1 == x and i2 == y:
+		global Mess,flag
+		Mess = "YEAH !!!! "
+		flag = True
+		
+	
 main()
 
-for i in range (15):
-	print("")
-for j in range (X):
-	print(j+1,end = " ")
-for i in range (Y):
-	print(i+1)
-	for j in range (X):
-		if (i == x and j == y ):
-			print (" 608 ", end = "")
-		else: print (" 609 ", end="")
-	print ('\n')
-for i in range(4):
-	print("")
-xi = int(input())
-yi = int(input())
-
-if (xi-1 == x and yi-1 == y):
-	print("yeah")
-else : print (" oh no " )
